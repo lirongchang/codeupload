b4ddf84e47d609593e7ab8f7774bb86e924e329d
oli <31873990+lirongchang@users.noreply.github.com>
2019-08-19 20:28:04 +0800

    在线编辑提交helloworld.c文件，在有上网策略拒绝时
    在线编辑提交helloworld.c文件，在有上网策略拒绝时

--- helloworld.c
+++ helloworld.c
@@ -5,5 +5,8 @@ int main()
  //第一次在线编辑提交helloworld.c文件，是在没有上网策略限制的前提下
  printf("在线编辑提交helloworld.c文件\n");
  printf("first commit helloworld.c with no rules\n");
+ //第二次在线编辑提交helloworld.c文件，是在有上网策略限制的前提下
+ printf("在线编辑提交helloworld.c文件，是在有上网策略限制的前提下进行的\n");
+ printf("在线编辑提交helloworld.c文件---------------");
  return 0;
 }
