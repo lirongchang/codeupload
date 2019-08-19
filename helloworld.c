a52c6e203d26fe1990b7e8f122582748255b44e3
oli <31873990+lirongchang@users.noreply.github.com>
2019-08-19 20:17:50 +0800

    第一次在线编辑提交helloworld.c文件
    第一次在线编辑提交helloworld.c文件，在没有上网策略限制的前提下进行的

--- helloworld.c
+++ helloworld.c
@@ -2,5 +2,8 @@
 int main()
 {
  printf("hello world\n");
+ //第一次在线编辑提交helloworld.c文件，是在没有上网策略限制的前提下
+ printf("在线编辑提交helloworld.c文件\n");
+ printf("first commit helloworld.c with no rules\n");
  return 0;
 }
