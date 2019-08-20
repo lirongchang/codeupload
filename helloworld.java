6ff3638c179adde31195db92cf60843f25b33d3e
oli <31873990+lirongchang@users.noreply.github.com>
2019-08-20 13:45:26 +0800

    开启SSL后，在线编辑提交代码
    开启SSL后，在线编辑提交代码

--- helloworld.java
+++ helloworld.java
@@ -2,4 +2,8 @@ public static void main(String []args){
  System.out.println("hello world");
  //策略拒绝时，编辑提交
  System.out.println("hello world ####################33");
+ //SSL 开启后在线编辑提交代码
+ System.out.println("hello world");
+ System.out.println("hello world");
+ System.out.println("hello world");
 }
